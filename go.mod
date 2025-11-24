a33ee1b03e0badb5ca86c784eea8c35915401b72
dkblackley <dblackle@gmu.edu>
2025-11-24 13:15:35 -0500

    Initial commit

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/bins-go
+module github.com/dkblackley/bins-go
 
 go 1.25.4
 
--- main.go
+++ main.go
@@ -1,29 +1,7 @@
-package bins_go
+package main
 
 import (
-	"crypto/sha256"
-	"encoding/binary"
-	"encoding/csv"
-	"encoding/json"
-	"flag"
-	"fmt"
-	"math"
-	"os"
-	"regexp"
-	"runtime"
-	"strconv"
-	"time"
-
-	"github.com/blugelabs/bluge"
-	"github.com/blugelabs/bluge/analysis"
-	"github.com/blugelabs/bluge/analysis/char"
-	"github.com/blugelabs/bluge/analysis/lang/en"
-	"github.com/blugelabs/bluge/analysis/token"
-	"github.com/blugelabs/bluge/analysis/tokenizer"
-	"github.com/dkblackley/bins-go/bins"
 	"github.com/dkblackley/bins-go/pianopir"
-	"github.com/schollz/progressbar/v3"
-	"github.com/sirupsen/logrus"
 )
 
 const MAX_UINT32 = ^uint32(0)
@@ -36,4 +14,8 @@ const RTT = 50
 type PIRImpliment interface {
 	intoDb() []uint64
 	getPIRInfo() *pianopir.SimpleBatchPianoPIR
+}
+
+func main() {
+	logrus.SetLevel(logrus.InfoLevel)
 }
