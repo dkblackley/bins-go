cc7c2b30d6e25496a933797e5d6e0d141fd9b021
dkblackley <dkblackley@gmail.com>
2026-01-04 16:47:39 +0000

    Update python file path

--- re_rank.py
+++ re_rank.py
@@ -101,9 +101,12 @@ def main(args):
     root = "../datasets/Son/"
     # step 4: load embeddings and rerank
     # pca = faiss.read_VectorTransform("pca_768_to_192.faiss")
-    query_embeddings, _ = load_queries_embeddings(root+'local_query.npy')#, 'queries_768.npy.ids')
+    # query_embeddings, _ = load_queries_embeddings(root+'local_query.npy')#, 'queries_768.npy.ids')
+    query_embeddings, _ = load_queries_embeddings(root+'../datasets/Son/query_192_float32.npy')#, 'queries_768.npy.ids')
     mrrs = []
-    document_embeddings, idmap = load_embeddings(root+'local_collection.npy', root+'my_vectors_768.npy.ids')
+    # document_embeddings, idmap = load_embeddings(root+'local_collection.npy', root+'my_vectors_768.npy.ids')
+    document_embeddings, idmap = load_embeddings(root+'../datasets/Son/my_vectors_192.npy', root+'my_vectors_768.npy.ids')
+
     print(document_embeddings.shape, idmap.shape)
     database = {}
     for id, embedding in tqdm(zip(idmap, document_embeddings), total=len(idmap)):
