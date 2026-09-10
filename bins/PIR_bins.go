b9680f88d3ff71f96940cc6844520b5706e39d66
Daniel Blackley <dkblackley@gmail.com>
2026-09-10 11:35:57 +0100

    fixed memory waste

--- bins/PIR_bins.go
+++ bins/PIR_bins.go
@@ -251,7 +251,7 @@ func MakeVecDb(config *globals.Args) VecBins {
 
 	stage2Batch := T
 	maxQuery := 5
-	if len(idRaw) > 10000 && T >= 100 {
+	if len(idRaw) < 10000 && T >= 100 {
 		// The second DB crashes because scifact is so small, as a result we reduce the 'maxquery' so there are less
 		// rounds of PIR for stage2.
 		maxQuery = 5
