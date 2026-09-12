b54e140ff1a96ff9ee3aaba46eb2e6a66a5b8e5e
Daniel Blackley <dkblackley@gmail.com>
2026-09-12 11:35:17 -0700

    Fixed RTT issues

--- bins/PIR_bins.go
+++ bins/PIR_bins.go
@@ -252,7 +252,7 @@ func MakeVecDb(config *globals.Args) VecBins {
 	//SCifact is such a small DB that if we make the batch size big enough, then PIR crashes (it cant
 	// make batches of a size big enough) so we have it do a fixed/globally known number of rounds.
 	stage2Batch := T / 10
-	maxQuery := 10
+	maxQuery := 30
 	if len(docMap) < 10000 {
 		// The second DB crashes because scifact is so small, as a result we reduce the 'maxquery' so there are less
 		// rounds of PIR for stage2.
